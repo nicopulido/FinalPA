02bd84edc3f798c38bbd40668f44d7b8d41911f9
nicopulido <nicopipe2010@gmail.com>
2024-05-24 21:21:37 -0500

    Arreglando todo

--- src/Vista/Vista.java
+++ src/Vista/Vista.java
@@ -1,19 +1,9 @@
 package Vista;
 
 public class Vista {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    
->>>>>>> parent of 1f4dacb (Test)
-=======
-    
->>>>>>> parent of cf44214 (Merge branch 'main' of https://github.com/nicopulido/FinalPA)
-=======
+
     public static void main(String[] args) {
         System.out.println("Hi");
     }
     
->>>>>>> parent of d208de2 (Reverting changes)
 }
