0008e66fe91e84d6eb6bd8ac5cb5d2df8eaad12a
nicopulido <nicopipe2010@gmail.com>
2024-05-24 21:17:28 -0500

    Update Vista.java

--- src/Vista/Vista.java
+++ src/Vista/Vista.java
@@ -2,7 +2,11 @@ package Vista;
 
 public class Vista {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     
 >>>>>>> parent of 1f4dacb (Test)
+=======
+    
+>>>>>>> parent of cf44214 (Merge branch 'main' of https://github.com/nicopulido/FinalPA)
 }
