aee42f31888223b771771e3bf6768a21bd9be67a
Luisfess <luisfe0530@gmail.com>
2024-05-26 22:39:02 -0500

    Login mejor

--- src/Vista/Login.java
+++ src/Vista/Login.java
@@ -29,10 +29,6 @@ public class Login extends javax.swing.JFrame {
     private void initComponents() {
 
         background = new javax.swing.JPanel();
-        jPanel1 = new javax.swing.JPanel();
-        labelLogo = new javax.swing.JLabel();
-        labelNomEmp = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         cajaUser = new javax.swing.JTextField();
         jSeparator2 = new javax.swing.JSeparator();
@@ -40,51 +36,15 @@ public class Login extends javax.swing.JFrame {
         jSeparator3 = new javax.swing.JSeparator();
         botonIngresoLogin = new javax.swing.JButton();
         cajaContraPass = new javax.swing.JPasswordField();
+        jLabel4 = new javax.swing.JLabel();
+        labelLogo = new javax.swing.JLabel();
+        labelNomEmp = new javax.swing.JLabel();
+        labelFondoLogo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         background.setBackground(new java.awt.Color(255, 255, 255));
         background.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jPanel1.setBackground(new java.awt.Color(51, 153, 255));
-
-        labelLogo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        labelLogo.setIcon(new javax.swing.ImageIcon("C:\\Users\\luisf\\OneDrive\\Documentos\\UD\\AVANZADA\\Logotipo.png")); // NOI18N
-        labelLogo.setText("jLabel1");
-
-        labelNomEmp.setFont(new java.awt.Font("Roboto Medium", 1, 18)); // NOI18N
-        labelNomEmp.setText("LS SU PROOVEDOR S.A.S.");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(29, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(labelNomEmp, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap())
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(labelLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(49, 49, 49))))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(94, 94, 94)
-                .addComponent(labelLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(48, 48, 48)
-                .addComponent(labelNomEmp, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(176, Short.MAX_VALUE))
-        );
-
-        background.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 0, 270, 470));
-
-        jLabel2.setFont(new java.awt.Font("Roboto Black", 1, 36)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel2.setText("ADMIN LOGIN");
-        background.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, 260, 40));
 
         jLabel1.setFont(new java.awt.Font("Roboto Black", 1, 14)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 0, 0));
@@ -133,15 +93,37 @@ public class Login extends javax.swing.JFrame {
         });
         background.add(cajaContraPass, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 290, 330, -1));
 
+        jLabel4.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel4.setFont(new java.awt.Font("Roboto Black", 1, 24)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\luisf\\Downloads\\favicon.png")); // NOI18N
+        jLabel4.setText("ADMIN LOGIN");
+        background.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 60, 220, 40));
+
+        labelLogo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        labelLogo.setIcon(new javax.swing.ImageIcon("C:\\Users\\luisf\\OneDrive\\Documentos\\UD\\AVANZADA\\Logotipo.png")); // NOI18N
+        labelLogo.setText("jLabel1");
+        background.add(labelLogo, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 110, 157, 106));
+
+        labelNomEmp.setBackground(new java.awt.Color(0, 0, 0));
+        labelNomEmp.setFont(new java.awt.Font("Roboto Medium", 1, 18)); // NOI18N
+        labelNomEmp.setForeground(new java.awt.Color(255, 255, 255));
+        labelNomEmp.setText("LS SU PROOVEDOR S.A.S.");
+        background.add(labelNomEmp, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 230, 229, 46));
+
+        labelFondoLogo.setIcon(new javax.swing.ImageIcon("C:\\Users\\luisf\\Downloads\\city.png")); // NOI18N
+        labelFondoLogo.setText("jLabel2");
+        background.add(labelFondoLogo, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 0, 280, 470));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(background, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(background, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
@@ -177,40 +159,7 @@ public class Login extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_cajaContraPassMousePressed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Login().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel background;
@@ -218,11 +167,11 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JPasswordField cajaContraPass;
     private javax.swing.JTextField cajaUser;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JLabel labelFondoLogo;
     private javax.swing.JLabel labelLogo;
     private javax.swing.JLabel labelNomEmp;
     // End of variables declaration//GEN-END:variables
