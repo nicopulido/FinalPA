69e0ae631ae74662cac18085f3f74c47d8c23bd4
nicopulido <nicopipe2010@gmail.com>
2024-05-27 22:47:34 -0500

    Agregué un método para crear productos

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -2,6 +2,8 @@ package Controlador;
 
 import Modelo.Bodega;
 import Modelo.Encargado;
+import Modelo.Producto;
+import Modelo.Proveedor;
 import Vista.Vista;
 
 public class Controlador {
@@ -41,6 +43,9 @@ public class Controlador {
         return encargado.getUsuario().equals(usuario) && encargado.getPin().equals(pin);
     }
     
-    //crear rama nicolas
+    public void crearProducto(String nombre, Proveedor proveedor){
+        Producto producto = new Producto(nombre,proveedor);
+        this.bodega.getProductos().add(producto);
+    }
     
 }
