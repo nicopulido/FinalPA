98a8b5ecfe53f374b0e893fe3328f794c2903382
nicopulido <117591203+nicopulido@users.noreply.github.com>
2024-05-28 22:39:26 -0500

    Add files via upload

---
+++ src/Constants.java
@@ -0,0 +1,13 @@
+package Constants;
+
+public class Constants {
+    // mysql database
+    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/test";
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "";
+
+    //TABLAS QUE DEBE TENER
+    public static final String DB_TABLE_PRODUCTOS = "productos";
+    public static final String DB_TABLE_CLIENTES = "clientes";
+    public static final String DB_TABLE_PROVEEDORES = "proveedores";
+}
