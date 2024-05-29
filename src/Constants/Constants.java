261afcbcfcd07052bcb3a047671f774312483b7e
nicopulido <nicopipe2010@gmail.com>
2024-05-28 22:23:26 -0500

    x

--- src/Constants/Constants.java
+++ src/Constants/Constants.java
@@ -2,9 +2,9 @@ package Constants;
 
 public class Constants {
     // mysql database
-    public static final String DB_URL = "jdbc:mysql://localhost/YOURDATABASENAME";
+    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/test";
     public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "YOURPASSWORD";
+    public static final String DB_PASSWORD = "";
 
     //TABLAS QUE DEBE TENER
     public static final String DB_TABLE_PRODUCTOS = "productos";
--- src/Controlador/Main.java
+++ src/Controlador/Main.java
@@ -2,5 +2,7 @@ package Controlador;
 
 public class Main {
         public static void main(String[] args) {
+            DBQuerys si = new DBQuerys();
+            si.allClientes();
         }
     }
