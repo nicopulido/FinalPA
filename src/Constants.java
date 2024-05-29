ebe19c3fcfbc2a9d732d9c8cbb0a6d14b6a4d049
nicopulido <117591203+nicopulido@users.noreply.github.com>
2024-05-28 22:40:44 -0500

    Update Constants.java

--- src/Constants.java
+++ src/Constants.java
@@ -2,9 +2,9 @@ package Constants;
 
 public class Constants {
     // mysql database
-    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/test";
+    public static final String DB_URL = "jdbc:mysql://id/nombrebasedatos";
     public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "";
+    public static final String DB_PASSWORD = "contrasena";
 
     //TABLAS QUE DEBE TENER
     public static final String DB_TABLE_PRODUCTOS = "productos";
